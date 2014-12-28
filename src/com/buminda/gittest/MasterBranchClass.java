dad55294bb353f53231c34c95e64cdbe6d058999
unknown <buminda@gmail.com>
2014-12-27 22:48:42 -0800

    master commit

---
+++ src/com/buminda/gittest/MasterBranchClass.java
@@ -0,0 +1,13 @@
+package com.buminda.gittest;
+
+/**
+ * Created by Buminda on 27/12/2014.
+ */
+public class MasterBranchClass {
+    // git push origin master
+    /**
+     * git remote -v , see the origin
+     * git branch -v , see branches
+     * Please merger this to GitTeset
+     */
+}
