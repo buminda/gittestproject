4cff7dcab7771ffa085a960ffd33e2b76b787352
unknown <buminda@gmail.com>
2014-12-27 22:15:17 -0800

    second commit

---
+++ src/com/buminda/gittest/GitTestJava.java
@@ -0,0 +1,17 @@
+package com.buminda.gittest;
+
+/**
+ * Created by Buminda on 27/12/2014.
+ */
+public class GitTestJava {
+    // origin
+
+    /**
+     * touch README.md
+     git init
+     git add README.md
+     git commit -m "first commit"
+     git remote add origin https://github.com/buminda/gittestproject.git
+     git push -u origin master
+     */
+}
